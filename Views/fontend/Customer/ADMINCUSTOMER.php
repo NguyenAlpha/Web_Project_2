e97c6e8535ad3f7b781d05d3b9202611761d5c80
TheKienfrvt <nguyenkienvt9@gmail.com>
2025-04-15 15:49:36 +0700

    kien

--- Views/fontend/Customer/ADMINCUSTOMER.php
+++ Views/fontend/Customer/ADMINCUSTOMER.php
@@ -1,40 +1,18 @@
-<!-- <?php
-include ('../../../Core/Database.php');
+ <?php
+include_once(__DIR__ . '/../../../Core/Database.php');
+include_once(__DIR__ .'../../partitions/fontend/headerAdmin.php');
+
+// include_once(__DIR__ . '/../../partitions/fontend/header.php');
 $sql = "SELECT * FROM customers";
 // $result = $->query($sql);
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Danh sách sản phẩm</title>
-  <style>
-    .product {
-      border: 1px solid #ccc;
-      padding: 10px;
-      margin: 10px;
-      display: inline-block;
-      width: 200px;
-      text-align: center;
-    }
-    .product img {
-      width: 100%;
-      height: auto;
-    }
-  </style>
-</head>
-<body>
-  <h2>Danh sách sản phẩm</h2>
 
-
-
-  $conn->close();
-  ?>
-</body>
-</html> -->
-<h1>danh sách khách hàng</h1>
+<h1>Danh sách khách hàng</h1>
 <table>
+<?php
 
-  <?php foreach($customer as $value):?>
+?>
+  <?php foreach( $customer as $value ):?>
     <tr>
       <td><?php echo $value['username']?></td>
       <td><?php echo $value['password']?></td>
