6a3606f2601a0953c0b3d8ef70679fc81029ad51
NhatNguyen <nhatnguyen27042005@gmail.com>
2025-04-28 18:30:43 +0700

    add login/register and error page

--- index.php
+++ index.php
@@ -1,24 +1,40 @@
 <?php 
-    // session_start();
+    session_start();
+    $username = '';
+    if(isset($_SESSION["user"])) {
+        $username = $_SESSION["user"]['username'];
+    }
 
-    // if(isset($_SESSION["username"])) {
-    //     $username = $_SESSION["username"];
-    // }
     require "./Core/Database.php";
     require "./Models/BaseModel.php";
-    
     require "./Controllers/BaseController.php";
-    
-    $controllerName = (ucfirst($_GET["controller"] ?? "home"))  . "Controller";
-    
-    require "./Controllers/".$controllerName.".php";
-    $controllerObject = new $controllerName;
+
+    $controller = $_GET["controller"] ?? "home";
     $action = $_GET["action"] ?? "index";
-    $controllerObject->$action();
-    if(isset($_GET["controller"]) && $_GET["controller"]=='admin') {
+
+    $controllerName = ucfirst($controller) . "Controller";
+    $controllerFile = "./Controllers/" . $controllerName . ".php";
+
+    // Kiểm tra xem file controller có tồn tại không
+    if (file_exists($controllerFile)) {
+        require $controllerFile;
+
+        // Kiểm tra method (action) có tồn tại không
+        if (method_exists($controllerName, $action) &&
+        class_exists($controllerName)) {
+            $controllerObject = new $controllerName;
+            $controllerObject->$action();
+            
+            if(isset($_GET["controller"]) && $_GET["controller"]=='admin') {
+            } else {
+                include "./Views/partitions/frontend/footer.php";
+            }
+        } else {
+            // Action không tồn tại
+            include "./Views/errors/404.php";
+        }
     } else {
-        include "./Views/partitions/frontend/footer.php";
+        // File controller không tồn tại
+        include "./Views/errors/404.php";
     }
 ?>
-
-
