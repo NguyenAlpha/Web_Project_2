3576441a3bbcc4c51bf079884e8a18b7cd8058d9
NhatNguyen <nhatnguyen27042005@gmail.com>
2025-05-12 04:31:49 +0700

    fix

--- admin/Models/Ordermodel.php
+++ admin/Models/Ordermodel.php
@@ -1,29 +1,37 @@
 
 <?php 
 class OrderModel extends BaseModel{
-      const  TABLE = "orders";
-public function getAllOrders() {
-    $sql = "SELECT * FROM orders ORDER BY MaDon DESC";
-    return $this->getByQuery($sql);
-}
-public function confirmOrderById($id) {
-    $sql = "UPDATE orders SET TrangThai = 'đã xác nhận' WHERE MaDon = ?";
-    $stmt = $this->conn->prepare($sql);
-    $stmt->bind_param("i", $id);
-    return $stmt->execute();
-}
-public function getOrderByUserID($userID) {
-    $sql = "SELECT *, listproduct.SoLuong AS SoLuongOrder, orders.TrangThai AS TrangThaiDon, orders.MaDon AS MaOrder FROM orders 
-    INNER JOIN listproduct ON listproduct.MaDon = orders.MaDon
-    INNER JOIN products ON products.MaSP = listproduct.MaSP 
-    WHERE UserID = $userID";
-    return $this->getByQuery($sql);
-}
-   public function deleteOrder($userID) {
-      return $this->delete(self::TABLE,'userID',$userID);
-   }
+    const  TABLE = "orders";
+    public function getAllOrders() {
+        $sql = "SELECT * FROM orders ORDER BY MaDon DESC";
+        return $this->getByQuery($sql);
+    }
 
+    public function confirmOrderById($id) {
+        $sql = "UPDATE orders SET TrangThai = 'đã xác nhận' WHERE MaDon = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("i", $id);
+        return $stmt->execute();
+    }
 
+    public function getOrderByUserID($userID) {
+        $sql = "SELECT *, listproduct.SoLuong AS SoLuongOrder, orders.TrangThai AS TrangThaiDon, orders.MaDon AS MaOrder FROM orders 
+        INNER JOIN listproduct ON listproduct.MaDon = orders.MaDon
+        INNER JOIN products ON products.MaSP = listproduct.MaSP 
+        WHERE UserID = $userID";
+        return $this->getByQuery($sql);
+    }
+
+    public function deleteOrder($userID) {
+        return $this->delete(self::TABLE,'userID',$userID);
+    }
+
+    public function updateOrderStatus($maDon, $trangThai) {
+        $sql = "UPDATE orders SET TrangThai = ? WHERE MaDon = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("si", $trangThai, $maDon);
+        return $stmt->execute();
+    }
 
 
 }
