137ee814167cbf64b5e6cf5b7073603c6e86f4f7
huytoan <huytoan318@gmail.com>
2025-05-11 16:31:38 +0700

    aa

--- database/tmdt.sql
+++ database/tmdt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th5 11, 2025 lúc 09:31 AM
+-- Thời gian đã tạo: Th5 11, 2025 lúc 11:31 AM
 -- Phiên bản máy phục vụ: 10.4.32-MariaDB
 -- Phiên bản PHP: 8.2.12
 
@@ -103,9 +103,6 @@ INSERT INTO `carts` (`ID`, `userID`, `MaSP`, `SoLuong`) VALUES
 (4, 3, 4, 2),
 (5, 3, 12, 1),
 (6, 3, 18, 3),
-(7, 4, 3, 1),
-(8, 4, 7, 2),
-(9, 4, 22, 1),
 (10, 5, 9, 1),
 (11, 5, 15, 4),
 (12, 6, 2, 1),
@@ -145,7 +142,8 @@ INSERT INTO `carts` (`ID`, `userID`, `MaSP`, `SoLuong`) VALUES
 (46, 17, 19, 2),
 (47, 17, 23, 1),
 (54, 2, 4, 1),
-(55, 2, 16, 3);
+(55, 2, 16, 3),
+(59, 4, 2, 2);
 
 -- --------------------------------------------------------
 
@@ -246,7 +244,17 @@ CREATE TABLE `laptopgamingdetails` (
 INSERT INTO `laptopgamingdetails` (`MaSP`, `ThuongHieu`, `GPU`, `CPU`, `RAM`, `DungLuong`, `KichThuocManHinh`, `DoPhanGiai`) VALUES
 (3, 'MSI', 'NVIDIA GeForce RTX 3050 6GB', 'Intel Core i5-13500H', '8GB', '1TB', '15.6 inch', '1920x1080'),
 (4, 'Lenovo', 'NVIDIA GeForce RTX 4060 8GB', 'AMD Ryzen 7 7435HS', '24GB', '512GB', '15.6 inch', '1920x1080'),
-(7, 'Gigabyte', 'NVIDIA GeForce RTX 4050 6GB', 'Intel Core i5-13500H', '8GB', '512GB', '15.6 inch', '1920x1080');
+(7, 'Gigabyte', 'NVIDIA GeForce RTX 4050 6GB', 'Intel Core i5-13500H', '8GB', '512GB', '15.6 inch', '1920x1080'),
+(49, 'Acer', 'NVIDIA GeForce RTX 4050 6GB GDDR6 VRAM, hỗ trợ 2560 NVIDIA CUDA Cores', 'Intel Core i7-14700HX (20 lõi / 28 luồng, 3.9 GHz, 5.5 GHz, 33 MB, Intel Smart Cache)', '32GB', '512GB', '16 inch', '2560x1600'),
+(51, 'Lenovo', 'NVIDIA® GeForce RTX™ 4060 8GB GDDR6, Boost Clock 2370MHz, TGP 140W', 'Intel® Core™ i7-14650HX, 16C (8P + 8E) / 24T, P-core 2.2 / 5.2GHz, E-core 1.6 / 3.7GHz, 30MB', '16GB', '512GB', '16 inch', '2560x1600'),
+(53, 'Acer', 'NVIDIA® GeForce RTX™ 4070 8GB GDDR6', 'Intel Core i7-13700HX 3.7GHz up to 5.0GHz, 16 Cores 24 Threads ,30 MB Intel Smart Cache', '16GB', '512GB', '16 inch', '2560x1600'),
+(54, 'Asus', 'NVIDIA® GeForce RTX™ 4060 Laptop GPU 2225Mhz* at 100W(2175MHz Boost Clock+50MHz O.C.,75W+25W Dynamic Boost) + AMD Radeon™ Graphics', 'AMD Ryzen™ AI 9 HX 370 Processor 2.0GHz (36MB Cache, up to 5.1GHz, 12 cores, 24 Threads); AMD Ryzen™ AI up to 81 TOPs', '16GB', '1TB', '14 inch', '2560x1600'),
+(56, 'HP', 'NVIDIA® GeForce RTX™ 4070 8GB GDDR6', 'Intel Core i9-14900HX up to 5.8Ghz, 36MB', '32GB', '1TB', '16 inch', '2560x1440'),
+(57, 'MSI', 'NVIDIA® GeForce RTX™ 4050 Laptop GPU 6GB GDDR6 Up to 2355MHz Boost Clock 105W Maximum Graphics Power with Dynamic Boost.', 'Intel® Core™ i7-14700HX (1.5GHz upto 5.5GHz, 20 cores 28 threads, 33 MB Intel® Smart Cache)', '16GB', '1TB', '16 inch', '1920x1200'),
+(58, 'MSI', 'NVIDIA® GeForce RTX™ 5070 Ti Laptop GPU 12GB GDDR7 up to 2220MHz Boost Clock 140W Maximum Graphics Power with Dynamic Boost. AI TOPs: 992 TOPS', 'Intel® Core™ Ultra 7 255HX (1.80Hz up to 5.20GHz, 30MB Cache) AI NPU TOPs: 13 TOPs', '16GB', '512GB', '16 inch', '2560x1600'),
+(59, 'Lenovo', 'NVIDIA® GeForce RTX™ 4070 8GB GDDR6, Boost Clock 2175MHz, TGP 115W', 'Intel® Core™ i9-14900HX, 24C (8P + 16E) / 32T, P-core 2.2 / 5.8GHz, E-core 1.6 / 4.1GHz, 36MB', '32GB', '1TB', '16 inch', '3200x3200'),
+(60, 'MSI', 'NVIDIA® GeForce RTX™ 3050 Laptop GPU Up to 1172.5MHz Boost Clock 45W Maximum Graphics Power with Dynamic Boost.', 'Intel Core i7-13620H (3.6GHz~4.9GHz) 10 Nhân 16 Luồng', '16GB', 'SSD NVMe PCIe 512GB Gen4x4 (Còn 1 slot 2.5\" SATA HDD)', '15,6', '1920x1080'),
+(61, 'Acer', 'NVIDIA® GeForce RTX™ 4060 8GB GDDR6', 'Intel® Core™ i9-13900H 2.6 GHz (24MB Cache, up to 5.4 GHz, 14 cores, 20 Threads)', '16GB', '512GB', '15.6 ', '1920 x 1080');
 
 -- --------------------------------------------------------
 
@@ -268,7 +276,12 @@ INSERT INTO `listproduct` (`MaDon`, `MaSP`, `SoLuong`) VALUES
 (2, 9, 2),
 (2, 8, 5),
 (3, 2, 1),
-(3, 4, 3);
+(3, 4, 3),
+(4, 3, 1),
+(4, 22, 1),
+(4, 4, 1),
+(5, 2, 1),
+(6, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -319,7 +332,10 @@ CREATE TABLE `orders` (
 INSERT INTO `orders` (`MaDon`, `UserID`, `DiaChi`, `NgayDat`, `NgayGiao`, `TongTien`, `TrangThai`, `ThanhToan`) VALUES
 (1, 5, 'abc', NULL, NULL, 0, 'chờ xác nhận', 'chuyển khoản'),
 (2, 2, '229 cao thăng, p13, q4, hcm', '2025-05-10 11:49:45', NULL, 32070000, 'chờ xác nhận', 'chuyển khoản'),
-(3, 2, '229 cao thăng, p13, q4, hcm', NULL, NULL, 94860000, 'chờ xác nhận', 'tiền mặt');
+(3, 2, '229 cao thăng, p13, q4, hcm', NULL, NULL, 94860000, 'chờ xác nhận', 'tiền mặt'),
+(4, 4, 'Biệt thự số 12, đường Lê Quý Đôn, Quận 3, TP.HCM', NULL, NULL, 56989000, 'chờ xác nhận', 'tiền mặt'),
+(5, 4, 'Biệt thự số 12, đường Lê Quý Đôn, Quận 3, TP.HCM', NULL, NULL, 11490000, 'chờ xác nhận', 'tiền mặt'),
+(6, 4, 'Biệt thự số 12, đường Lê Quý Đôn, Quận 3, TP.HCM', NULL, NULL, 11490000, 'chờ xác nhận', 'tiền mặt');
 
 -- --------------------------------------------------------
 
@@ -364,7 +380,18 @@ INSERT INTO `products` (`MaSP`, `TenSP`, `MaLoai`, `AnhMoTaSP`, `SoLuong`, `DaBa
 (21, 'Card màn hình Asus ROG Strix GeForce RTX 4090 OC Edition 24GB GDDR6X', 'GPU', './assets/image/tn9pvbdr.png', 10, 15, 64990000, 'hiện'),
 (22, 'VGA Gigabyte RTX 4060 Windforce OC 8GB', 'GPU', './assets/image/45659_vga_gigabyte_rtx_4060_windforce_oc_8gb_anphat88.jpg', 100, 41, 8299000, 'hiện'),
 (23, 'VGA Gigabyte GeForce RTX 3050 WINDFORCE OC V2 8GB', 'GPU', './assets/image/46200_vga_gigabyte_geforce_rtx_3050_windforce___2_.jpg', 12, 4, 5599000, 'hiện'),
-(24, 'Laptop Dell Latitude 3450 71058806', 'Laptop', './assets/image/51342_laptop_dell_latitude_3450_71058806__1_.jpg', 100, 53, 24990000, 'hiện');
+(24, 'Laptop Dell Latitude 3450 71058806', 'Laptop', './assets/image/51342_laptop_dell_latitude_3450_71058806__1_.jpg', 100, 53, 24990000, 'hiện'),
+(49, 'Laptop gaming Acer Predator Helios Neo 16 PHN16 72 78L4', 'LaptopGaming', './assets/image/1746952951_acer_predator_helios_neo_16_2024__2__3ffd04967bc44b82b78f3e0cee408665_1024x1024.jpg', 50, 0, 38490000, 'hiện'),
+(51, 'Laptop gaming Lenovo Legion 5 16IRX9 83DG004YVN', 'LaptopGaming', './assets/image/1746953416_legion_5_16irx9_ct1_01_6639fb2c9ce446439a36578865a5c7d0_1024x1024.jpg', 20, 0, 37990000, 'hiện'),
+(53, 'Laptop gaming Acer Predator Helios Neo 16 PHN16 71 74QR', 'LaptopGaming', './assets/image/1746953641_468f7a1472eb8f563424b86621d_c04206638f4643d58ab3079d6dc42ec0_1024x1024_18da0285e1e848248921bbcfc8c80c53_grande.jpg', 23, 0, 41990000, 'hiện'),
+(54, 'Laptop gaming ASUS TUF Gaming FA401WV RG062WS', 'LaptopGaming', './assets/image/1746954077_ava_dea980b662854ab8a4dd359d3bd8d2b4_grande.jpg', 21, 0, 39990000, 'hiện'),
+(55, 'HP Pavilion Gaming 15 EC2158AX', 'LaptopGaming', './assets/image/1746954382_HP-Pavilion-Gaming-15-Ryzen-03.jpg', 38, 0, 38600000, 'hiện'),
+(56, 'Laptop gaming HP OMEN 16-wf1137TX A2NR9PA', 'LaptopGaming', './assets/image/1746954618_1_a4de5185d81a4d8e851974281003b6d4_grande.jpg', 25, 0, 52490000, 'hiện'),
+(57, 'Laptop gaming MSI Sword 16 HX B14VEKG 856VN', 'LaptopGaming', './assets/image/1746954844_ava_ecb79fdbde454bfd87bf7ccd8675e972_grande.jpg', 15, 0, 31490000, 'hiện'),
+(58, 'Laptop gaming MSI Vector 16 HX AI A2XWHG 010VN', 'LaptopGaming', './assets/image/1746955011_1024__1__035bd6ee5a8246078c525b4bc8d2e55b_grande.jpg', 7, 0, 54990000, 'hiện'),
+(59, 'Laptop gaming Lenovo Legion 7 16IRX9 83FD004MVN', 'LaptopGaming', './assets/image/1746955134_ava_1388feab03cd40a2ad5b495d909a0a60_grande.jpg', 12, 0, 59990000, 'hiện'),
+(60, 'Laptop gaming MSI Thin 15 B13UC 2044VN', 'LaptopGaming', './assets/image/1746955252_thin-new_d31ff3b88e7f40e7ac88acc624e03d4f_grande.jpg', 22, 0, 19290000, 'hiện'),
+(61, 'Laptop gaming Acer Nitro V ANV15 51 91T5', 'LaptopGaming', './assets/image/1746955427_nitro-v_755588bd95514b6386940d73d3951e2d_1024x1024_95ef516ce29440e4ad51dedbab0e352c_grande.jpg', 11, 0, 32990000, 'hiện');
 
 -- --------------------------------------------------------
 
@@ -507,19 +534,19 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT cho bảng `carts`
 --
 ALTER TABLE `carts`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
 
 --
 -- AUTO_INCREMENT cho bảng `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `MaDon` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `MaDon` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT cho bảng `products`
 --
 ALTER TABLE `products`
-  MODIFY `MaSP` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `MaSP` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
 
 --
 -- AUTO_INCREMENT cho bảng `users`
